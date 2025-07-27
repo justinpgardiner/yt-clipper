b6f5a0383c45e70d96513b693abcc94b467ad7da
justinpgardiner <justinpgardiner@gmail.com>
2025-07-27 15:22:02 -0400

    clean ui and add data collection to clip_video view

--- clips/urls.py
+++ clips/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("dashboard/", views.dashboard, name="dashboard"),
     path("build/", views.build, name="build"),
-    path("clip_video/", views.build, name="clip_video"),
+    path("clip_video/", views.clip_video, name="clip_video"),
     path("accounts/", include('accounts.urls')),
     path('', include('home.urls')),
 ]
--- clips/views.py
+++ clips/views.py
@@ -9,7 +9,20 @@ def build(request):
 
 @login_required
 def clip_video(request):
+    clip_url = None
     if request.method == 'POST':
-        name = request.POST.get('name')
-        units = request.POST.get('units')
-    return render(request, 'clips/build.html')
+        url = request.POST.get('url')
+        number_of_rings = request.POST.get('number_of_rings')
+        color_of_rings = request.POST.get('color_of_rings')
+        color_of_ball = request.POST.get('color_of_ball')
+        color_of_subtitles = request.POST.get('color_of_subtitles')
+        font_size = request.POST.get('font_size')
+        try:
+            font_size = int(font_size) if font_size else None
+        except ValueError:
+            font_size = None
+
+        # Do your video clipping logic here...
+        clip_url = 'videos/cjG0m9QUF340asmr_fixed.mp4'
+
+    return render(request, 'clips/build.html', {'clip_url': clip_url})
