5ad14104030b5e1dc6087e7c7dbb62cd7400ec73
justinpgardiner <justinpgardiner@gmail.com>
2025-07-26 21:38:46 -0400

    more messy progress towards UI for build

--- clips/views.py
+++ clips/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 def dashboard(request):
     return render(request, 'clips/dashboard.html')
 
 def build(request):
+    return render(request, 'clips/build.html')
+
+@login_required
+def clip_video(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        units = request.POST.get('units')
     return render(request, 'clips/build.html')
